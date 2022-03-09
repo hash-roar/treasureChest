31f6a55ecc9e699cdc91a504a1fd80bcbbdec1ab
hash-roa-onecho <1022643754@qq.com>
2022-03-09 22:39:41 +0800

    add actix-web

--- rust/shorturl/src/main.rs
+++ rust/shorturl/src/main.rs
@@ -1,11 +1,29 @@
-#[macro_use] extern crate rocket;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey",web::get().to(manual_hello))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
