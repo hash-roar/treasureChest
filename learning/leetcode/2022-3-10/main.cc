a9e3e6c19296aceccc7f240930be38cabdf6690c
hash-roar <1022647354@qq.com>
2022-03-10 15:38:26 +0800

    binary tree

--- learning/leetcode/2022-3-10/main.cc
+++ learning/leetcode/2022-3-10/main.cc
@@ -1,6 +1,51 @@
-
 #include "../include.h"
+#include <algorithm>
+#include <queue>
+using namespace std;
 
+DouDimensionArr traverse_tree_level(TreeNode* root)
+{
+  auto result =DouDimensionArr{};
+  if (root==nullptr) {
+    return result;
+  }
+  auto que = queue<TreeNode*>{};
+  que.push(root);
+  while (!que.empty()) {
+    int len = que.size();
+    auto temp = IntArr{};
+    for(int i=0;i<len;i++)
+    {
+      if(que.front()->left!=nullptr)
+      {
+        que.push(que.front()->left);
+      }else if (que.front()->right!=nullptr) {
+        que.push(que.front()->right);
+      }
+
+      temp.push_back(que.front()->val);
+      que.pop();
+    }
+    result.push_back(move(temp));
+  }
+  return result;
+}
+
+int get_h_index(IntArr const& vec)
+{
+  auto arr = vec;
+  sort(arr.begin(),arr.end());
+  int h_inedx =0;
+  for(int i=arr.size();i>0;i--)
+  {
+    if (arr[i]>arr.size()-i) {
+      h_inedx++;
+    }else {
+    break;
+    }
+  }
+  return h_inedx;
+}
 
 
 
