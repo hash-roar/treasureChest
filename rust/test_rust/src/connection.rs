ce58203a2384a6e70233f70cc3e29b4dfbf77226
hash-roar <1022647354@qq.com>
2022-06-10 21:36:15 +0800

    delete some unsed code and add more logf

--- rust/test_rust/src/connection.rs
+++ rust/test_rust/src/connection.rs
@@ -1,8 +1,9 @@
-use std::net::{ SocketAddr};
+use std::{net::{ SocketAddr}, sync::Mutex, fmt::{Display, self}};
 
 use color_eyre::Result;
-use tokio::{net::TcpStream, io::{BufReader, BufWriter}, select};
-use tracing::instrument;
+use tokio::{sync::oneshot,net::{TcpStream}, io::{BufReader, BufWriter, copy, copy_bidirectional}, select};
+use tracing::{instrument,warn, info};
+
 
 #[derive(Debug)]
 pub enum ProxyState {
@@ -10,31 +11,57 @@ pub enum ProxyState {
     FromConnSet,
     ToConnSetting,
     Established,
-    ShutDowning,
-    Error
 }
+
+impl  ProxyState {
+    fn to_string(&self)-> &str{
+        match self {
+            &ProxyState::Init=> "connection initing",
+            &ProxyState::FromConnSet=>"connect from src attached",
+            &ProxyState::ToConnSetting=>"connecting to the des",
+            &ProxyState::Established=>"connection established",
+        }
+    } 
+}
+
+impl fmt::Display for ProxyState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.to_string()
+        )
+    }
+}
+
 
 #[derive(Debug)]
 pub struct  ProxyConnection{
-    pub state:ProxyState,
+    pub state:Mutex<ProxyState>,
 }
 
 impl ProxyConnection {
     pub fn new()-> ProxyConnection{
         ProxyConnection{
-            state: ProxyState::Init
+            state: Mutex::new(ProxyState::Init)
         }
     }
 
     #[instrument]
-     pub async fn process(&mut self,mut tcp:TcpStream,addr: SocketAddr,des :String) -> Result<()>{
-        let (reader, mut writer ) = tcp.split();
-        let mut reader = BufReader::new(reader);
-        let mut writer = BufWriter::new(writer);
-
-        select! {
-            
+     pub async fn process(& self,mut tcp:TcpStream,addr: SocketAddr,des :String) -> Result<()>{
+        {
+            *self.state.lock().unwrap() = ProxyState::ToConnSetting;
         }
+
+        let mut des_stream = TcpStream::connect(&des).await?;
+
+        {
+            *self.state.lock().unwrap() = ProxyState::Established;
+        }
+        let (src_to_des, des_to_src)=  copy_bidirectional(&mut tcp, & mut des_stream).await?;
+
+        info!("connection: {} ===> {}  close,copy bytes: src --> des:{},  des --> src:{}",
+            addr,&des,src_to_des,des_to_src);
         Ok(())
     }
 
--- rust/test_rust/src/proxy.rs
+++ rust/test_rust/src/proxy.rs
@@ -1,52 +1,75 @@
-use std::sync::atomic::{self, AtomicU64};
+use std::{sync::{atomic::{self, AtomicU64}, Arc, Mutex}, collections::HashMap};
 
 use color_eyre::Result;
 use tokio::net::TcpListener;
-use tracing::{info, instrument};
+use tracing::{info, warn};
 
 use crate::{
     config::Config,
     connection::{ProxyConnection, ProxyState},
 };
 
+type  ProxyConnsPtr = Arc<Mutex<HashMap<u64,Arc<ProxyConnection>>>>;
+
 #[derive(Debug)]
 pub struct ProxyServer {
     pub config: Config,
     pub total_counter: atomic::AtomicU64,
-    pub connection_counter: atomic::AtomicU64,
+    pub connection_counter: Arc<atomic::AtomicU64>,
+    pub connections : ProxyConnsPtr
 }
 
-impl ProxyServer {
+impl ProxyServer {  
     pub fn new(config: Config) -> Self {
         ProxyServer {
             config: config,
-            total_counter: AtomicU64::new(0),
-            connection_counter: AtomicU64::new(0),
+            total_counter:AtomicU64::new(0),
+            connection_counter: Arc::new(AtomicU64::new(0)) ,
+            connections:Arc::new(Mutex::new(HashMap::new()))
         }
     }
 
-    async fn start(&mut self) -> Result<()> {
+    pub async fn collect_info(conn_ptr : ProxyConnsPtr) -> Result<()>{
+        let (connecting,connected) =(0,0);
+
+        Ok(())
+    }
+    
+
+    pub async fn start(&self) -> Result<()> {
         let listener = TcpListener::bind(&self.config.src).await?;
+        info!("ProxyServer Start at: {}  ===> {}",&self.config.src,&self.config.des);
 
         loop {
-            let mut conn = ProxyConnection::new();
+            let conn = Arc::new( ProxyConnection::new());
             let des = self.config.des.clone();
             let (tcp_stream, addr) = listener.accept().await?;
-            conn.state = ProxyState::FromConnSet;
 
+            let conn_counter = self.connection_counter.clone();
+            let  conns = self.connections.clone();
+            conn_counter.fetch_add(1, atomic::Ordering::Relaxed);   
+            {
+                *conn.state.lock().unwrap()= ProxyState::FromConnSet;
+                // to unlock 
+            }
+            {
+                let mut conns = conns.lock().unwrap();
+                conns.insert(self.total_counter.fetch_add(1, atomic::Ordering::Acquire), conn.clone());
+            }
             tokio::spawn(async move {
                 let result = conn.process(tcp_stream, addr, des).await;
 
+                // self.total_counter.fetch_sub(1, atomic::Ordering::Relaxed);
                 // TODO()
+
+                conn_counter.fetch_sub(1, atomic::Ordering::Relaxed);
                 match result {
                     Ok(_) => {}
                     Err(err) => {
-                        info!("{}", err.to_string());
+                        warn!("{}", err.to_string());
                     }
                 }
             });
         }
-
-        Ok(())
     }
 }
